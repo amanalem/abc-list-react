7d2a3080a428a9cfad3f5ea4cb8ead438857092a
aman-alem <alem.aman@gmail.com>
2022-08-11 22:51:45 -0500

    Browser Router set on index.js page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState({});
+
+  const [listA, setListA] = useState([]);
+
+  const [listB, setListB] = useState([]);
+
+  const [listC, setListC] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_DB}/my-list`);
+  });
+
+  return <div className="App">Hello Verld</div>;
 }
 
 export default App;
