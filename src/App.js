dc3892c175589c5772d9bcd380dda0136a98da7e
aman-alem <alem.aman@gmail.com>
2022-08-11 23:03:39 -0500

    page components added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
+import ListPage from "./pages/ListPage";
 
 function App() {
   const [user, setUser] = useState({});
@@ -17,7 +18,23 @@ function App() {
     axios.get(`${process.env.REACT_APP_DB}/my-list`);
   });
 
-  return <div className="App">Hello Verld</div>;
+  return (
+    <div className="App">
+      Hello Verld
+      <Routes>
+        <Route
+          path="/my-list"
+          element={
+            <ListPage
+              setListA={setListA}
+              setListB={setListB}
+              setListC={setListC}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
